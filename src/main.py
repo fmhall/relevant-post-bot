0e5562e8c7244fc8f864fc9698c11c806e67406b
Mason <mason.hall@bloxroute.com>
2020-06-30 15:18:02 -0500

    final formatting changes

--- src/main.py
+++ src/main.py
@@ -33,8 +33,8 @@ CERTAINTY_THRESHOLD = 0.50
 SIMILARITY_THRESHOLD = 0.40
 
 BOT_TAG = (
-    "^I ^am ^a ^bot ^created ^by [^fmhall](https://www.reddit.com/user/fmhall), ^inspired ^by [^(this "
-    "comment.)]({})\n\n".format(
+    "^I ^am ^a ^bot ^created ^by [^(fmhall,)](https://www.reddit.com/user/fmhall) ^inspired ^by [^(this "
+    "comment.)]({})\n".format(
         "https://www.reddit.com/r/AnarchyChess/comments/durvcj/dude_doesnt_play_chess_thinks_he_can_beat_magnus/f78cga9"
     )
 )
